4de5dc73404b727bbb3d75e3410cc2483e1f8fdb
Samantha Montgomery <montgomerysamantha5@gmail.com>
2018-11-13 13:33:15 -0600

    Added the start of a get_sign() function

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "date.h"
 #include "zodiac.h"
 
+char * get_sign(int month, int day); //returns the sign of the given date
+
 int main() {
   printf("The month is %s, abrev is %s, number is %d, days is %d\n",
    january.name,
@@ -24,31 +26,61 @@ int main() {
 
   printf("The test date is %s %d\n", test.month, test.day);
 
-  if (test.monthNumber == 1)
-  {
-    if (test.day < 20)
-    {
-      //capricorn
-    }
-    else
-    {
-      //aquarius
-      sign = aquarius.name;
-    }
-  }
-  else if (test.monthNumber == 2)
-  {
-    if (test.day < 19)
-    {
-      //aquarius
-      sign = aquarius.name;
-    }
-    else
-    {
-      //pisces
-    }
-  }
+  sign = get_sign(test.monthNumber, test.day); //get_sign function returns their sign
 
   printf("The person born on %s %d would be an %s\n", test.month, test.day, sign);
   return 0;
+}
+
+char * get_sign(int month, int day)
+{
+  char * sign;
+  switch (month)
+  {
+    case 1:
+      if (day < 20)
+      {
+        sign = "Capricorn";
+      }
+      else
+      {
+        sign = "Aquarius";
+      }
+      break;
+    case 2:
+      if (day < 19)
+      {
+        sign = "Aquarius";
+      }
+      else
+      {
+        sign = "Pisces";
+      }
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    case 9:
+      break;
+    case 10:
+      break;
+    case 11:
+      break;
+    case 12:
+      break;
+    default:
+      printf("Invalid month, unable to determine sign.");
+      break;
+  }
+
+  return sign;
 }
