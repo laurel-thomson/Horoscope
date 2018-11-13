dc840cc5fe9301b9d11e0e0756c418be63172624
Samantha Montgomery <montgomerysamantha5@gmail.com>
2018-11-13 14:09:18 -0600

    get_sign() function is now complete, moved to sign.h file

--- main.c
+++ main.c
@@ -4,15 +4,14 @@
 #include "month.h"
 #include "date.h"
 #include "zodiac.h"
-
-char * get_sign(int month, int day); //returns the sign of the given date
+#include "sign.h"
 
 int main() {
-  printf("The month is %s, abrev is %s, number is %d, days is %d\n",
+  /*printf("The month is %s, abrev is %s, number is %d, days is %d\n",
    january.name,
    january.abrev,
    january.monthNumber,
-   january.days);
+   january.days);*/
 
   /* aquarius */
   zodiac aquarius = {
@@ -22,65 +21,27 @@ int main() {
 
   date test = { "January", 1, 21 }; //a sample day
 
-  char *sign; //the sign of the person which we will update later
+  //char *sign; //the sign of the person which we will update later
 
-  printf("The test date is %s %d\n", test.month, test.day);
+  //printf("The test date is %s %d\n", test.month, test.day);
 
-  sign = get_sign(test.monthNumber, test.day); //get_sign function returns their sign
+  //sign = get_sign(test.monthNumber, test.day); //get_sign function returns their sign
 
-  printf("The person born on %s %d would be an %s\n", test.month, test.day, sign);
+  //printf("The person born on %s %d would be an %s\n", test.month, test.day, sign);
+
+  int birthmonth;
+  printf("Enter the month of your birthday (example: May would be 5): ");
+  scanf("%d", &birthmonth);
+  printf("Birth month is %d\n", birthmonth);
+  printf("\n");
+
+  int birthday;
+  printf("Enter the day of your birthday (example: 16): ");
+  scanf("%d", &birthday);
+  printf("Total bithday is %d/%d\n", birthmonth, birthday);
+  printf("\n");
+
+  char *newsign = get_sign(birthmonth, birthday);
+  printf("Your sign is a %s!\n", newsign);
   return 0;
-}
-
-char * get_sign(int month, int day)
-{
-  char * sign;
-  switch (month)
-  {
-    case 1:
-      if (day < 20)
-      {
-        sign = "Capricorn";
-      }
-      else
-      {
-        sign = "Aquarius";
-      }
-      break;
-    case 2:
-      if (day < 19)
-      {
-        sign = "Aquarius";
-      }
-      else
-      {
-        sign = "Pisces";
-      }
-      break;
-    case 3:
-      break;
-    case 4:
-      break;
-    case 5:
-      break;
-    case 6:
-      break;
-    case 7:
-      break;
-    case 8:
-      break;
-    case 9:
-      break;
-    case 10:
-      break;
-    case 11:
-      break;
-    case 12:
-      break;
-    default:
-      printf("Invalid month, unable to determine sign.");
-      break;
-  }
-
-  return sign;
 }
