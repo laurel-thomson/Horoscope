17dfe43b7197c45b38effc0cece1b5c0b8b6a6bf
laurel-thomson <laurel228@ksu.edu>
2018-11-26 21:24:02 -0600

    Combining the two is_valid_date functions into 1 function

--- date.h
+++ date.h
@@ -137,6 +137,25 @@ boolean is_valid_date(int m, int d)
 	if (m < 0 || m > 12) return false;
 	if (d < 0 || d > 31) return false;
 	Month *current_month = monthsOfYear[m-1];
+	
+	//special case for February & leap year
+	if (m == 2 && d == 29)
+	{
+		//check if leap year
+        int year;
+        printf("What is the year of your birthday? (ex: 2012)\n");
+        scanf("%d", &year);
+		
+		if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) //is it a leap year
+        {
+			return true; //valid leap year date
+        }
+        else
+        {
+            return false; //not a leap year :(
+        }
+
+	}
 	if (current_month->days < d) return false;
 	return true;
 }
@@ -157,61 +176,6 @@ Date * get_current_date()
 	int month = tm->tm_mon + 1;
 	int day = tm->tm_mday;
 	return get_date(month, day);
-} 
-
-
-boolean is_valid_date(date d)
-{
-  if (d.monthNumber >= 1 && d.monthNumber <= 12) //is it between jan and dec?
-  {
-    //now check the dates
-    switch (d.monthNumber)
-    {
-      case 1: //month numbers w/ 31 days
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-        if (d.day >= 1 && d.day <= 31) return true;
-        else return false;
-        break;
-      case 4: //month numbers w/ 30 days
-      case 6:
-      case 9:
-      case 11:
-        if (d.day >= 1 && d.day <= 30) return true;
-        else return false;
-        break;
-      case 2: //special case for february
-        if (d.day >= 1 && d.day <= 28) return true;
-        else if (d.day == 29) //is day feb. 29?
-        {
-          //check if leap year
-          int year;
-          printf("What is the year of your birthday? (ex: 2012)\n");
-          scanf("%d", &year);
-
-          if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) //is it a leap year
-          {
-            return true; //valid leap year date
-          }
-          else
-          {
-            return false; //not a leap year :(
-          }
-        }
-        else
-        {
-          return false;
-        }
-        break;
-      default:
-        break;
-    }
-  }
-  return true;
 }
 
 #endif
