c9fa27054e2555877d2a5a5feee42c818c2e962e
Laurel Thomson <laurel228@cougar.cs.ksu.edu>
2018-11-30 10:32:03 -0600

    Restructuring csv format to be simplified and fixing get_fortune method to correctly read in file

--- fortune.h
+++ fortune.h
@@ -9,51 +9,45 @@ typedef enum Moon {
 	FULL
 } Moon;
 
-//the fortunes are hard-coded in for now - will need to update this to read from a CSV
 char * fortunes[2][12];
 
-const char * get_fortune(int zodiac, int moon)
+char * test_get_fortune(int x, int y)
 {
-	return fortunes[moon][zodiac];
+	return fortunes[x][y];
 }
 
-//this is hard-coded in for now as well - will need to read in from moon phases CSV
-Moon get_moon(Date * date)
+char * get_fortune(Zodiac * zodiac, int moon)
 {
-	if (date->day < 16) return NEW;
-	return FULL;
+	return fortunes[moon][zodiac->index];
 }
 
-//Code to read in from CSV file
 void readFile(char * filename){
-   if(filename == "" || filename == NULL){printf("No file name given\n");}
-   else{
-	strtok(filename, "\n");
-	//printf("Creating fortune from default file: %s\n", filename);
+	if (filename == "" || filename == NULL)
+	{
+		printf("No file name given\n");
+		return;
+	}
+
 	FILE * f = fopen(filename, "r");
    	char str[1024];
 
-   	if(f == NULL){
+   	if(f == NULL)
+	{
 	    printf("Error reading file\n");
+		return;
 	}
- 	else{
-	   for(int i = 1; i <= 2; i++){
-	      for(int j = 1; j <= 12; j++){
-	         fortunes[i][j] = malloc(sizeof(char) * 1024);
-		 fgets(str, 1024, f);
-		 char * input[3];
-		 int i = 0;
-	         for(char * token = strtok(str, ","); token != NULL; token = strtok(NULL, ",")){
-		    input[i] = token;
-		    i++;
-		 }
-	         fortunes[i][j] = input[2];
-		 printf("%s\n", fortunes[i][j]);
-	      }
-	   }
-	}
-   fclose(f);
+	
+   for(int i = 0; i < 2; i++)
+   {
+		for(int j = 0; j <= 11; j++)
+		{  
+			fortunes[i][j] = malloc(sizeof(char) * 1024);
+			fgets(str, 1024, f);
+			strcpy(fortunes[i][j],str);
+			printf("x: %d, y: %d\n %s\n\n",i,j, fortunes[i][j]);
+		}
    }
+   fclose(f);
 }
 
 //Method to free inputted fortune data
--- main.c
+++ main.c
@@ -10,80 +10,23 @@
 char * DEFAULT_FORTUNE_FILE = "MoonPhaseFortunes.csv";
 
 int main() {
-
-	printf("Welcome to your own personal ~~Fortune Teller~~\n\n");
-	printf("Would you like to specify your own fortune CSV file? (y)es or (n)o: ");
-	char c;
-	scanf(" %c", &c);
-
-	if (c == 'y' || c == 'Y')
-	{
-		//read in from a csv of the user's choice
-		char filename[MAX_FILENAME_LENGTH];
-		printf("What file would you like to read from? ");
-		scanf("%s",filename);
-		readFile(filename);
-	}
-	else if(c == 'n' || c == 'N')
-	{
-		//read in from the default file
-		readFile(DEFAULT_FORTUNE_FILE);
-	}
-	else{printf("Invalid option");}
-
+	
+	readFile(DEFAULT_FORTUNE_FILE);
+	
 	while (true)
 	{
-		printf("Would you like to have your fortune told? (y)es or (n)o: ");
-		char a;
-		scanf(" %c",&a);
-		if (a == 'n' || a == 'N')
-		{
-			printf("Sorry to see you go!\n");
-			break;
-		}
-		int birthmonth;
-		printf("Enter the month of your birthday (example: May would be 5): ");
-		scanf("%d", &birthmonth);
-
-		int birthday;
-		printf("Enter the day of your birthday (example: 16): ");
-		scanf("%d", &birthday);
-
-		boolean is_valid = is_valid_date(birthmonth, birthday);
-
-		if (is_valid)
-		{
-			Date * birth_date = get_date(birthmonth, birthday);
-			Date * current_date = get_current_date();
-			Zodiac * zodiac = get_sign(birth_date);
-
-			printf("Your sign is a %s!\n", zodiac->name);
-
-			Moon moon = get_moon(current_date);
-
-			char * moon_string;
-
-			if (moon == 0)
-			{
-				moon_string = "new";
-			}
-			else
-			{
-				moon_string = "full";
-			}
-
-			printf("Today is %d/%d and it is a %s moon!\n",current_date->month->monthNumber,current_date->day,moon_string);
-
-			char * fortune = get_fortune(zodiac, moon); //Try calling getfortune(#,#)
-			printf("Your fortune is %s\n", fortune);
-
-			free(birth_date);
-			free(current_date);
-			free(zodiac);
-		}
-		else printf("You have not entered a valid date!\n");
+		int x;
+		printf("Enter x: ");
+		scanf(" %d",&x);
+		
+		int y;
+		printf("Enter y: ");
+		scanf(" %d",&y);
+		
+		char * fortune = test_get_fortune(x,y);
+		
+		printf("%s\n",fortune);
 	}
-	//call function to free fortune memory
 	freeData();
 	return 0;
 }
